f90fc191cd84b627c65f958b9096040435d81916
Lucas Fryzek <Nercos7@gmail.com>
2012-12-20 09:13:06 -0500

    Inital Commit

---
+++ dsz/DSZ.java
@@ -0,0 +1,31 @@
+package dsz;
+
+import org.jsfml.window.*;
+import org.jsfml.window.event.*;
+import org.jsfml.graphics.*;
+
+public class DSZ {
+	
+	static String title = "Dungeons, Swords & Zombies";
+
+	public static void main(String[] args) {
+		RenderWindow screen = new RenderWindow(new VideoMode(800,600),title);
+		screen.setFramerateLimit(60);
+		
+		while(screen.isOpen()){
+			for(Event event : screen.pollEvents()){
+				if(event.type == Event.Type.CLOSED){
+					screen.close();
+				}
+				
+			}
+			
+			
+			screen.clear(Color.CYAN);
+			
+			screen.display();
+		}
+
+	}
+
+}
